0bcafa042e517778fda81c0e313d45f5651c7309
KaoruOnuki <kaoru.onuki@gmail.com>
2018-12-21 15:07:14 +0900

    added seed data

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,5 +1,3 @@
-$(document).on('turbolinks:load', function(){
-    $(document).ready(function() {
-      $("#hoge").css("color","red")
-    });
-});
+$(document).ready(function(){
+  $("#hoge").css("color","red")
+}
